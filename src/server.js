b6e55b47fad1b8429cc63550e3f0ebaf7fa48603
Hala <130187284+HalaaAlii@users.noreply.github.com>
2024-05-05 21:46:28 -0400

    sad

--- src/server.js
+++ src/server.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-
 // server used to send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+
+const PORT = process.env.PORT || 4000; // Use process.env.PORT if available, otherwise use port 4000
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
