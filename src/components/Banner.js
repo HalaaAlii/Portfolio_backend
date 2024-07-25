0d736dd7823946029816c0fcc382b3a9e3f4cd66
Hala <130187284+HalaaAlii@users.noreply.github.com>
2024-07-25 12:14:43 -0400

    pic chnae

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/6923331-removebg-preview.png";
+import headerImg from "../assets/img/PIRATES_2_-_nikao-removebg-preview.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import TrackVisibility from 'react-on-screen';
 
